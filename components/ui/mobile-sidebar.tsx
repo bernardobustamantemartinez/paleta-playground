7561fcfa74953c1a7a9390a7420814a2498e3d4e
Bernardo <bernardobustamantemartinez@gmail.com>
2023-11-29 16:46:24 -0800

    Dashboard page

--- components/ui/mobile-sidebar.tsx
+++ components/ui/mobile-sidebar.tsx
@@ -8,6 +8,14 @@ import Sidebar from "../sidebar";
 
 const MobileSidebar = () => {
   const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
   return (
     <Sheet>
       <SheetTrigger>
