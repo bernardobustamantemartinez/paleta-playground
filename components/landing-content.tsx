6c2ef93574396a5408bcc108c0973a2035ec631f
Bernardo <bernardobustamantemartinez@gmail.com>
2023-12-07 14:04:04 -0800

    Card styling + font

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -29,14 +29,14 @@ const testimonials = [
 export const LandingContent = () => {
   return (
     <div className="px-10 pb-20">
-      <h2 className="text-center text-4xl text-white font-extraboldmb-10">
+      <h2 className="text-center text-4xl text-white font-extrabold mb-10">
         Testimonials
       </h2>
       <div className="grid grid-cols-1 sm:grild-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {testimonials.map((item) => (
           <Card
             key={item.description}
-            className="bg-#[192339] border-none text-white"
+            className="border-none text-white bg-[#192339]"
           >
             <CardHeader>
               <CardTitle className="flex items-center gap-x-2">
@@ -45,7 +45,7 @@ export const LandingContent = () => {
                   <p className="text-zinc-400 text-sm">{item.title}</p>
                 </div>
               </CardTitle>
-              <CardContent className="pt-4 px-0">
+              <CardContent className="pt-4 px-0 font-medium">
                 {item.description}
               </CardContent>
             </CardHeader>
