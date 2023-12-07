308c9f785f6dcf0eead4a9a54d95c6774eaeebf4
Bernardo <bernardobustamantemartinez@gmail.com>
2023-12-07 14:19:01 -0800

    lowercase

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -10,7 +10,7 @@ export const LandingHero = () => {
   return (
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
-        <h1>Explore different AI Models in one place.</h1>
+        <h1>Explore different AI models in one place.</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-purple-400">
           <TypewriterComponent
             options={{
